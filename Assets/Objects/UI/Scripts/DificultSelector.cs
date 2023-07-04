83e0d6a96d7c4a90e385b49d25a54527743b3c15
Владислав <wwwxumuk@mail.ru>
2023-07-04 17:58:09 +0300

    Add LoadingWindow; Изменен баланс наград за уровень сложности

--- Assets/Objects/UI/Scripts/DificultSelector.cs
+++ Assets/Objects/UI/Scripts/DificultSelector.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DificultSelector : MonoBehaviour
 {
     [SerializeField] private Characteristics _easyCharacteristics;
     [SerializeField] private Characteristics _mediumCharacteristics;
     [SerializeField] private Characteristics _hardCharacteristics;
+
+    [SerializeField] private int _mediumCost;
+    [SerializeField] private int _hardCost;
+
+    [SerializeField] private IntValueViewer _mediumCostViewer;
+    [SerializeField] private IntValueViewer _hardCostViewer;
+
+    [SerializeField] private GameObject _mediumCostEffect;
+    [SerializeField] private GameObject _hardCostEffect;
+
+    [SerializeField] private Button _mediumButton;
+    [SerializeField] private Button _hardButton;
+
     [SerializeField] private EnemyCharacteristics _enemyCharacteristics;
+
+    [SerializeField] private RewardWidget _rewardWidget;
+
+    private Data _data;
+    private int _winRewardModify = 2;
 
     private void Awake()
     {
         Time.timeScale = 0.0f;
+        _data = Data.Instance;
+    }
+
+    private void Start()
+    {
+        SetCost();
+        CheckEnoughtGold();
     }
 
     public void SetEasyMod()
@@ -20,19 +46,45 @@ public class DificultSelector : MonoBehaviour
 
     public void SetMediumMod()
     {
-        _enemyCharacteristics.SetCharacteristics(_mediumCharacteristics);
-        CloseWindow();
+        if (_data.TrySpendGold(_mediumCost))
+        {
+            _enemyCharacteristics.SetCharacteristics(_mediumCharacteristics);
+
+            _rewardWidget.SetWinReward(_mediumCost * _winRewardModify);
+
+            CloseWindow();
+        }
     }
 
     public void SetHardMod()
     {
-        _enemyCharacteristics.SetCharacteristics(_hardCharacteristics);
-        CloseWindow();
+        if (_data.TrySpendGold(_hardCost))
+        {
+            _enemyCharacteristics.SetCharacteristics(_hardCharacteristics);
+
+            _rewardWidget.SetWinReward(_hardCost * _winRewardModify);
+
+            CloseWindow();
+        }
     }
 
     private void CloseWindow()
     {
         Time.timeScale = 1.0f;
         gameObject.SetActive(false);
+    }
+
+    private void SetCost()
+    {
+        _mediumCostViewer.SetValue(_mediumCost);
+        _hardCostViewer.SetValue(_hardCost);
+    }
+
+    private void CheckEnoughtGold()
+    {
+        _mediumCostEffect.SetActive(_data.Gold >= _mediumCost);
+        _mediumButton.interactable = _data.Gold >= _mediumCost;
+        _hardCostEffect.SetActive(_data.Gold >= _hardCost);
+        _hardButton.interactable = _data.Gold >= _hardCost;
     }
 }
--- Assets/Scripts/RewardWidget.cs
+++ Assets/Scripts/RewardWidget.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class RewardWidget : MonoBehaviour
 {
-    [SerializeField] private int _winReward = 1000;
-    [SerializeField] private int _loseReward = 20;
+    [SerializeField] private int _winReward = 100;
+    [SerializeField] private int _loseReward = 0;
     [SerializeField] private IntValueViewer _rewardViewer;
+    [SerializeField] private IntValueViewer _advRewardViewer;
+    [SerializeField] private int _modifyAdvWinReavard = 3;
 
+    private int _advWinReavard;
     private int _reward;
+    private int _modifyAdvReavard;
 
     public void Reward(bool isWin)
     {
         _reward = isWin ? _winReward : _loseReward;
+        _modifyAdvReavard = isWin ? _modifyAdvWinReavard : 1;
+        _advWinReavard = _winReward * _modifyAdvReavard;
         _rewardViewer.SetValue(_reward);
+        _advRewardViewer?.SetValue(_advWinReavard);
         Data.Instance.AddGold(_reward);
+    }
+
+    public void SetWinReward(int value)
+    {
+        _winReward = value;
     }
 }
